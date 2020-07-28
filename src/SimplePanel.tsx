1666ee66721cd90eb6df2ccfdfbf3ce6f20ad503
Stefen Hasselberg <Stefen.Hasselberg@emergeaotearoa.org.nz>
2020-07-28 14:00:18 +1200

    my first grafana plugin

---
+++ src/SimplePanel.tsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import { PanelProps } from '@grafana/data';
+import { SimpleOptions } from 'types';
+import { css, cx } from 'emotion';
+import { stylesFactory, useTheme } from '@grafana/ui';
+
+interface Props extends PanelProps<SimpleOptions> {}
+
+export const SimplePanel: React.FC<Props> = ({ options, data, width, height }) => {
+  const theme = useTheme();
+  const styles = getStyles();
+  const values = data.series
+    .map(series => series.fields.find(field => field.name === 'Value'))
+    .map(field => field?.values.get(field.values.length - 1));
+
+  return (
+    <div
+      className={cx(
+        styles.wrapper,
+        css`
+          width: ${width}px;
+          height: ${height}px;
+        `
+      )}
+    >
+      <img src={`http://openweathermap.org/img/wn/${values[0]}@2x.png`} />
+      <h3>{values[1]}</h3>
+      <pre>{JSON.stringify(values, null, 2)}</pre>
+    </div>
+  );
+};
+
+const getStyles = stylesFactory(() => {
+  return {
+    wrapper: css`
+      position: relative;
+    `,
+    svg: css`
+      position: absolute;
+      top: 0;
+      left: 0;
+    `,
+    textBox: css`
+      position: absolute;
+      bottom: 0;
+      left: 0;
+      padding: 10px;
+    `,
+  };
+});
